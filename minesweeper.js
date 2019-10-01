238e71abc012d84e979092d7b1aba6e9a4d2a8d3
rlyhan <richard_han@live.com>
2019-10-01 19:13:08 +1300

    Board working

--- minesweeper.js
+++ minesweeper.js
@@ -1,7 +1,18 @@
 document.addEventListener('DOMContentLoaded', startGame)
 
 // Define your `board` object here!
-// var board = 
+var board = {}
+board.cells = []
+for (var i = 0; i < 3; i++) {
+  for (var j = 0; j < 3; j++) {
+    board.cells.push({
+      row: i,
+      col: j,
+      isMine: false,
+      hidden: true
+    })
+  }
+}
 
 function startGame () {
   // Don't remove this function call: it makes the game work!
